e80cb5e1310cbc111a8948f0de77ef154d3595cc
armel50 <armelfli50@gmail.com>
2022-10-16 17:40:48 -0500

    added google map in the app

--- lib/views/rooms/rooms.dart
+++ lib/views/rooms/rooms.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Room extends StatefulWidget {
   const Room({Key? key}) : super(key: key);
@@ -13,8 +11,15 @@ class Room extends StatefulWidget {
 class _RoomState extends State<Room> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Room/Listing')),
-    );
+    return Stack(children: [
+      GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(37.773972, -122.431297),
+          zoom: 12,
+        ),
+      )
+    ]);
   }
 }
