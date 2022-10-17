aba103ddb06098d101d93d330840651cd150a2d8
armel50 <armelfli50@gmail.com>
2022-10-17 11:36:38 -0500

    Now we can determine when the user is typing. if the user is typing the inputField will change

--- lib/views/messages/components/messageInputField.dart
+++ lib/views/messages/components/messageInputField.dart
@@ -1,9 +1,15 @@
 import 'package:chance_app/constants.dart';
 import 'package:flutter/material.dart';
 
-class MessageInput extends StatelessWidget {
+class MessageInput extends StatefulWidget {
   const MessageInput({Key? key}) : super(key: key);
 
+  @override
+  State<MessageInput> createState() => _MessageInputState();
+}
+
+class _MessageInputState extends State<MessageInput> {
+  var isTyping = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,16 +18,17 @@ class MessageInput extends StatelessWidget {
         color: Colors.white,
         boxShadow: [
           BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 10,
-                    blurRadius: 32,
-                    offset: Offset(3, 3),)
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 10,
+            blurRadius: 32,
+            offset: Offset(3, 3),
+          )
         ],
       ),
       child: SafeArea(
         child: Row(
           children: [
-                IconButton(
+            IconButton(
               onPressed: () {
                 print('pressed');
               },
@@ -39,38 +46,63 @@ class MessageInput extends StatelessWidget {
                 child: Row(
                   children: [
                     Expanded(
-                      
                       child: TextField(
                         cursorColor: AppBarTextColor,
-                        
-                        // maxLines: 4,
+                        keyboardType: TextInputType.multiline,
+                        minLines: 1, //Normal textInputField will be displayed
+                        maxLines: 5,
                         decoration: InputDecoration(
                           hintStyle: TextStyle(color: AppBarTextColor),
                           hintText: "Type message",
                           border: InputBorder.none,
                         ),
                         style: TextStyle(color: AppBarTextColor),
+                        onChanged: (val) {
+                          if (val.length > 0) {
+                            setState(() {
+                              isTyping = true;
+                            });
+                          } else {
+                             setState(() {
+                            isTyping = false;
+                            });
+                          }
+                        },
                       ),
                     ),
-                   
                   ],
                 ),
               ),
             ),
-                               
-
-            IconButton(
-              onPressed: () {
-                print('pressed');
-              },
-              icon: Icon(Icons.mic, color: primaryColor),
-            ),
-            IconButton(
-              onPressed: () {
-                print('pressed');
-              },
-              icon: Icon(Icons.camera_alt_outlined, color: primaryColor),
-            ),
+            if (isTyping)
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: defaultPadding,
+                ),
+                margin: EdgeInsets.only(left: defaultMargin),
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: primaryColor),
+                child: IconButton(
+                  onPressed: () {
+                    print('pressed');
+                  },
+                  icon: Icon(Icons.send_rounded, color: AppBarTextColor),
+                ),
+              ),
+            if (!isTyping)
+              IconButton(
+                onPressed: () {
+                  print('pressed');
+                },
+                icon: Icon(Icons.mic, color: primaryColor),
+              ),
+            if (!isTyping)
+              IconButton(
+                onPressed: () {
+                  print('pressed');
+                },
+                icon: Icon(Icons.camera_alt_outlined, color: primaryColor),
+              ),
           ],
         ),
       ),
